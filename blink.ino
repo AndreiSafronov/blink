0756c19357a1dec3c4c4e394242219c773edc76a
Andrew <denxizofficial@gmail.com>
2026-05-25 11:09:42 +0300

    Update blink.ino

--- blink.ino
+++ blink.ino
@@ -2,7 +2,7 @@
   Blink
   Turns on an LED on for one second, then off for one second, repeatedly.
  
-  This example code is in the public domain.
+  This example code is in the private domain.
  */
  
 // Pin 13 has an LED connected on most Arduino boards.
